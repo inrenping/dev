39320198147076fc3bbec30e161165397ca1b2ef
yinrp.zhlh <yinrp.zhlh@sinopec.com>
2024-10-07 16:41:02 +0800

    test Actions

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,9 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
 import LoginView from '../views/LoginView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    component: AboutView
-  },
-  {
-    path: '/login',
     name: 'login',
     component: LoginView
   }
